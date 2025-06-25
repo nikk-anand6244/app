f180d1e19fd441a460fb665ed43c99ad76c63a4c
nikk-anand6244 <160687776+nikk-anand6244@users.noreply.github.com>
2025-06-25 11:25:26 +0530

    Adding binding in Gradle  module app file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,11 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures.viewBinding=true
+    buildFeatures.buildConfig=true
+    buildFeatures.dataBinding=true
+
 }
 
 dependencies {
